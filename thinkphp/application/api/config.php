ab326e4dc23f547b780e1184ef232fa37e24fe57
kefong <529539000@qq.com>
2017-10-13 08:15:51 +0800

    add thinkphp db cookie config

--- thinkphp/application/api/config.php
+++ thinkphp/application/api/config.php
@@ -1,5 +1,21 @@
 <?php
 //配置文件
 return [
-
+		'default_return_type'	=> 'json',
+		'cookie'	=> [
+				// cookie 名称前缀
+				'prefix'    => 'kefong_api_',
+				// cookie 保存时间
+				'expire'    => 0,
+				// cookie 保存路径
+				'path'      => '/',
+				// cookie 有效域名
+				'domain'    => '',
+				//  cookie 启用安全传输
+				'secure'    => true,
+				// httponly设置
+				'httponly'  => '',
+				// 是否使用 setcookie
+				'setcookie' => true,
+		],
 ];
